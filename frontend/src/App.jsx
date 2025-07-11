af0a55b8657bd09a2317fa07f1165b13f6c33db4
vikramnreddy02 <vikram.n-reddy-ext@hpe.com>
2025-07-11 10:55:27 +0000

    latest changes:please dont change

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,13 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import Dashboard from './pages/Dashboard';
-<<<<<<< HEAD
-=======
 import Header from './components/Header';
-import Footer from './components/Footer';
->>>>>>> 255326d (Final)
+import Footer from './components/Footer'
 import './App.css';
 
 export default function App() {
   return (
     <BrowserRouter basename="/">
-<<<<<<< HEAD
-      <Routes>
-        <Route path="/"          element={<Login />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-      </Routes>
-=======
       <div className="app-layout">
         <Header />
         <div className="app-main">
@@ -27,7 +18,6 @@ export default function App() {
         </div>
         <Footer />
       </div>
->>>>>>> 255326d (Final)
     </BrowserRouter>
   );
 }
